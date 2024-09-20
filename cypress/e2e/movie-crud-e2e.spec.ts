3dae3e1cc55a26b2392980d3e57a4238c26121d1
Murat Ozcan <muratkerem@gmail.com>
2024-09-20 14:18:13 -0500

    no mock:server

--- cypress/e2e/movie-crud-e2e.spec.ts
+++ cypress/e2e/movie-crud-e2e.spec.ts
@@ -1,10 +1,19 @@
 import '@cypress/skip-test/support'
 import {generateMovie} from '@support/factories'
+import {retryableBefore} from '@support/retryable-before'
 import spok from 'cy-spok'
 
 describe('movie crud e2e', () => {
-  it('should add and delete a movie', () => {
+  retryableBefore(() => {
+    // skip in CI, for sure the server is not running
     cy.task('isCi').then(val => cy.skipOn(val === true))
+    // when local, skip if the server is not running
+    cy.exec(`curl -s -o /dev/null -w "%{http_code}" ${Cypress.env('apiUrl')}`, {
+      failOnNonZeroExit: false,
+    }).then(res => cy.skipOn(res.stdout !== '200'))
+  })
+
+  it('should add and delete a movie', () => {
     cy.intercept('GET', '/movies').as('getMovies')
     cy.visit('/')
     cy.contains('Movie List')
