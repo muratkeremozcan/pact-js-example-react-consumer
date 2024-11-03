7da368ece7620700b5f77e7b726400def1c520a8
Murat Ozcan <muratkerem@gmail.com>
2024-11-03 06:35:51 -0600

    fix: minor corrections part7

--- src/hooks/use-movie-detail.ts
+++ src/hooks/use-movie-detail.ts
@@ -3,7 +3,9 @@ import {useParams, useSearchParams} from 'react-router-dom'
 
 export function useMovieDetails() {
   // Get the id from the route params or query parameters
+  // .../movies/{{movieId}}
   const {id} = useParams<{id: string}>()
+  // .../movies?name={{movieName}}
   const [searchParams] = useSearchParams()
   const movieName = searchParams.get('name')
 
