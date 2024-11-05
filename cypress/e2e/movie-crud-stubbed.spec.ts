97022a504f26e2aa3f0bc6d0b2ea99719f178c9f
Murat Ozcan <muratkerem@gmail.com>
2024-11-05 08:06:52 -0600

    feat: breaking change, movie director

--- cypress/e2e/movie-crud-stubbed.spec.ts
+++ cypress/e2e/movie-crud-stubbed.spec.ts
@@ -1,14 +1,16 @@
 import {generateMovie} from '@support/factories'
+import {addMovie} from '@support/helpers/add-movie'
 import {editMovie} from '@support/helpers/edit-movie'
 
 describe('movie crud e2e', () => {
-  const {name, year, rating} = generateMovie()
+  const {name, year, rating, director} = generateMovie()
   const id = 1
   const movie = {id, name, year, rating}
   const {
     name: editedName,
     year: editedYear,
     rating: editedRating,
+    director: editedDirector,
   } = generateMovie()
 
   it('should add a movie', () => {
@@ -16,33 +18,23 @@ describe('movie crud e2e', () => {
     cy.visit('/')
     cy.wait('@noMovies')
 
-    cy.getByCy('movie-input-comp-text').type(name, {delay: 0})
-    cy.get('[placeholder="Movie year"]')
-      .clear()
-      .type(`${year}{backspace}`, {delay: 0})
-    cy.get('[placeholder="Movie rating"]').clear().type(`${rating}`, {
-      delay: 0,
-    })
-    cy.intercept('POST', '/movies', {
-      body: {
-        ...movie,
-      },
-    }).as('addMovie')
-    cy.intercept('GET', '**/movies', {body: [movie]}).as('getMovies')
+    addMovie(name, year, rating, director)
 
+    cy.intercept('POST', '/movies', {body: movie}).as('addMovie')
+    cy.intercept('GET', '**/movies', {body: {data: [movie]}}).as('getMovies')
     cy.getByCy('add-movie-button').click()
-    cy.wait('@getMovies')
     cy.wait('@addMovie')
+    cy.wait('@getMovies')
   })
 
   it('should edit a movie', () => {
     cy.intercept('GET', '**/movies', {body: {data: [movie]}}).as('getMovies')
     cy.intercept('GET', '**/movies/*', {body: {data: movie}}).as('getMovieById')
+
     cy.visit('/')
     cy.wait('@getMovies')
 
     cy.getByCy(`link-${id}`).click()
-
     cy.location('pathname').should('eq', `/movies/${id}`)
     cy.wait('@getMovieById').its('response.body.data').should('deep.eq', movie)
 
@@ -52,10 +44,12 @@ describe('movie crud e2e', () => {
         name: editedName,
         year: editedYear,
         rating: editedRating,
+        director: editedDirector,
       },
     }).as('updateMovieById')
 
-    editMovie(editedName, editedYear, editedRating)
+    editMovie(editedName, editedYear, editedRating, editedDirector)
+    cy.getByCy('movie-input-comp-text').should('have.attr', 'value', editedName)
 
     cy.wait('@updateMovieById')
   })
