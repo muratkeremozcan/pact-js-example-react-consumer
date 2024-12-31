b1f375ae3dac5e2da37dcd7b3f877a8bde24186d
muratkeremozcan <muratkerem@gmail.com>
2024-12-31 07:10:24 -0600

    test: course refinements-5

--- src/components/error-component.vitest.tsx
+++ src/components/error-component.vitest.tsx
@@ -2,9 +2,9 @@ import {describe, expect, it, screen, wrappedRender} from '@vitest-utils/utils'
 import ErrorComponent from './error-component'
 
 describe('<ErrorComponent />', () => {
-  it('should render error message', () => {
+  it('should render an error message', () => {
     wrappedRender(<ErrorComponent />)
-
     expect(screen.getByTestId('error')).toBeVisible()
+    expect(screen.getByText('Try reloading the page.'))
   })
 })
