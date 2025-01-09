58e1906eabf361924f71c256933d160eff736458
muratkeremozcan <muratkerem@gmail.com>
2025-01-09 16:19:53 -0600

    test: component test enhancements

---
+++ src/components/movie-details/movie-edit-form.vitest.tsx
@@ -0,0 +1,46 @@
+import {
+  wrappedRender,
+  screen,
+  worker,
+  http,
+  describe,
+  it,
+  expect,
+  userEvent,
+  vi,
+} from '@vitest-utils/utils'
+import MovieEditForm from './movie-edit-form'
+import {generateMovie} from '@cypress/support/factories'
+import type {Movie} from 'src/consumer'
+
+describe('<MovieEditForm />', () => {
+  const id = 7
+  const movie: Movie = {id, ...generateMovie()}
+
+  it('should cancel and submit a movie update', async () => {
+    const onCancel = vi.fn()
+
+    let putRequest: Record<string, unknown> | undefined
+    worker.use(
+      http.put(`http://localhost:3001/movies/${id}`, async ({request}) => {
+        const requestBody = await request.json()
+        putRequest = requestBody as Record<string, unknown>
+        return new Response(undefined, {status: 200})
+      }),
+    )
+
+    wrappedRender(<MovieEditForm movie={movie} onCancel={onCancel} />)
+
+    await userEvent.click(screen.getByTestId('cancel'))
+    expect(onCancel).toHaveBeenCalledOnce()
+
+    await userEvent.click(screen.getByTestId('update-movie'))
+
+    expect(putRequest).toMatchObject({
+      name: movie.name,
+      year: movie.year,
+      rating: movie.rating,
+      director: movie.director,
+    })
+  })
+})
