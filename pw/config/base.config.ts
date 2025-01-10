3cccc1153082eb3746b2c7d950d272237b8820de
muratkeremozcan <muratkerem@gmail.com>
2025-01-10 07:01:14 -0600

    base config

--- pw/config/base.config.ts
+++ pw/config/base.config.ts
@@ -5,7 +5,7 @@ export const baseConfig = defineConfig({
   testMatch: '**/*.spec.ts',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 4 : 1,
+  retries: process.env.CI ? 3 : 2,
   workers: process.env.CI ? 1 : undefined,
   reporter: process.env.CI ? 'html' : 'list',
   timeout: process.env.CI ? 15000 : 5000,
